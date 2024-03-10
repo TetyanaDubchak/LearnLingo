a0d7acc44654889eba5c519d7da83e0fe8e30541
TetyanaDubchak <123674629+TetyanaDubchak@users.noreply.github.com>
2024-03-10 19:47:39 +0000

    Deploying to gh-pages from @ TetyanaDubchak/LearnLingo@98cdf632a3eedcb8cf67aae3aa52528027187dba 🚀

---
+++ static/js/395.27cefdb6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[395],{7395:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)(a.Fragment,{})}}}]);
+//# sourceMappingURL=395.27cefdb6.chunk.js.map
